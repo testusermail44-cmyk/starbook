57d330529f9218f71b56020e5dc27520f97cc3fb
testusermail44-cmyk <testusermail44@gmail.com>
2026-04-23 21:23:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,64 +21,66 @@ session_start();
   ?>
   <div class="back"></div>
   <div class="slider hc">
-    <div class="slider-btn left">&lt;</div>
-    <div class="slider-content">
-      <?php foreach ($types as $t) { ?>
-        <div class="slider-item">
-          <img class="slider-img" src="public/images/objects/<?= $t['image'] ?>">
-          <div class="slider-t title"><?= $t['name'] ?></div>
-          <div class="slider-t t"><?= $t['description'] ?></div>
+    <div class="slider-btn left">&lt;
+      </div>
+        <div class="slider-content">
+          <?php
+          foreach ($types as $t) { ?>
+            <div class="slider-item">
+              <img class="slider-img" src="public/images/objects/<?= $t['image'] ?>">
+              <div class="slider-t title"><?= $t['name'] ?></div>
+              <div class="slider-t t"><?= $t['description'] ?></div>
+            </div>
+            <?php
+          }
+          ?>
         </div>
-      <?php } ?>
+        <div class="slider-btn right">&gt;</div>
     </div>
-    <div class="slider-btn right">&gt;</div>
-  </div>
-
   <script>
-    const items = document.querySelectorAll('.slider-item');
-    const leftBtn = document.querySelector('.slider-btn.left');
-    const rightBtn = document.querySelector('.slider-btn.right');
-    const slider = document.querySelector('.slider.hc');
+  const items = document.querySelectorAll('.slider-item');
+  const leftBtn = document.querySelector('.slider-btn.left');
+  const rightBtn = document.querySelector('.slider-btn.right');
+  const slider = document.querySelector('.slider.hc');
 
-    let index = 0;
-    let interval;
+  let index = 0;
+  let interval;
 
-    function updateSlider() {
-      items.forEach((i, idx) => {
-        i.classList.toggle('active', idx === index);
-      });
-    }
+  function updateSlider() {
+    items.forEach((i, idx) => {
+      i.classList.toggle('active', idx === index);
+    });
+  }
 
-    function next() {
-      index++;
-      if (index >= items.length) index = 0;
-      updateSlider();
-    }
-
-    function prev() {
-      index--;
-      if (index < 0) index = items.length - 1;
-      updateSlider();
-    }
-
-    rightBtn.addEventListener('click', next);
-    leftBtn.addEventListener('click', prev);
-
-    function startAuto() {
-      stopAuto();
-      interval = setInterval(next, 5000);
-    }
-
-    function stopAuto() {
-      if (interval) clearInterval(interval);
-    }
-
-    slider.addEventListener('mouseenter', stopAuto);
-    slider.addEventListener('mouseleave', startAuto);
-
+  function next() {
+    index++;
+    if (index >= items.length) index = 0;
     updateSlider();
-    startAuto();
-  </script>
-</body>
+  }
 
+  function prev() {
+    index--;
+    if (index < 0) index = items.length - 1;
+    updateSlider();
+  }
+
+  rightBtn.addEventListener('click', next);
+  leftBtn.addEventListener('click', prev);
+
+  function startAuto() {
+    stopAuto();
+    interval = setInterval(next, 5000);
+  }
+
+  function stopAuto() {
+    if (interval) clearInterval(interval);
+  }
+
+  slider.addEventListener('mouseenter', stopAuto);
+  slider.addEventListener('mouseleave', startAuto);
+
+  updateSlider();
+  startAuto();
+</script>
+</body>
 </html>
