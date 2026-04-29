940f774b53ead6d835c2c256ef74735e3434dcc7
testusermail44-cmyk <testusermail44@gmail.com>
2026-04-29 22:42:14 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,11 @@
 <?php
-session_start();
-$_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+$_SESSION = [];
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 86400, '/');
 }
 session_destroy();
-header("Location: /index.php");
+header("Location: ../index.php");
 exit;
